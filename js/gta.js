5481cdce41ef98122346cf03fe70d671cbd55ae0
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-05-17 18:33:10 +0700

    Add files via upload

--- js/gta.js
+++ js/gta.js
@@ -1,17 +1,57 @@
 const gtadata = [
-     {
-        "title": "Backup GTA ",
-        "version": "ALL ",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-        "description": "  ✅ Lấy xe ✅ nhân vật ✅ máu ✅ tiền ✅ vũ khí ",
-        "download_url": "https://drive.google.com/uc?export=download&id=1Na9o0tZN1eXkqu8ZfpNeX_Y5W8yGgIUL",
-        "updated_at": "2025-05-15",
-        "size": "0.19 MB"
+        {
+        "title": "Back Up GTA ( dân tổ,naruto )",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
+        "description": "✅Back Up GTA ( dân tổ,naruto )\n  ✅ Lấy xe ✅ nhân vật ✅ máu ✅ tiền ✅ vũ khí ",
+        "download_url": "https://drive.google.com/uc?export=download&id=188i4e4Ju467TOsRjh9TtoaVcTeBSFn5k",
+        "version": "1.0",
+        "updated_at": "2025-05-17",
+        "size": "0.06 MB"
     },
-      {
+    {
+        "title": "Back Up GTA ( boy phố )",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "description": "✅Back Up GTA ( boy phố ) ✅ Lấy xe ✅ nhân vật ✅ máu ✅ tiền ✅ vũ khí ",
+        "download_url": "https://drive.google.com/uc?export=download&id=1lEHpocSGnVigPlsABMNo7Kq3Ht3iD6R1",
+        "version": "1.0",
+        "updated_at": "2025-05-17",
+        "size": "0.06 MB"
+    },
+        {
+        "title": "GTA San F17",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/F17.jpg",
+        "description": "✅Mod xe Full Dream  \n✅mod Nhân vật Boy phố   , Còi, Pô ",
+        "download_url": "https://drive.google.com/uc?export=download&id=1vqTesp9Venw8MsIirkX0w41ygofZKXDR",
+        "version": "2.02.11",
+        "updated_at": "2025-05-17",
+        "size": "1.85 GB"
+    },
+        {
+        "title": "GTA San F16",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/F16.jpg",
+        "description": "✅Mod xe Full sonic  \n✅mod Nhân vật Boy Phố  , Còi, Pô ",
+        "download_url": "https://drive.google.com/uc?export=download&id=1t-WvrJp7H7wzE0yMEEcylZJ16GyH5PPZ",
+        "version": "2.02.11",
+        "updated_at": "2025-05-17",
+        "size": "1.98 GB"
+    },
+       {
+        "title": "GTA: SAN V2.2",
+        "version": "2.02.07",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_9521.jpeg",
+        "description": "✅Mod xe dream, khối phân lớn\n✅mod Nhân vật , Còi, Pô, cờ VN, cảnh sát \n",
+        "download_url": "https://drive.google.com/uc?export=download&id=1ffXKKLpTfx9yUWl00FTEr-6DjKRSSd4a",
+        "updated_at": "2025-05-15",
+        "size": "1.89 GB"
+    },
+    
+    {
         "title": "GTA: SAN V2.1",
         "version": "2.02.07",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b2.jpg",
         "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V2.1.jpg",
         "description": "✅Mod xe wave \n✅mod Nhân vật naruto  , Còi, Pô, Phóng , cảnh sát \n",
         "download_url": "https://drive.google.com/drive/folders/12CtCIqaHeHqvrN7ZDuWL68xaZ_X2NFie?usp=drive_link",
@@ -19,89 +59,74 @@ const gtadata = [
         "size": "1.89 GB"
     },
       {
-        "title": "GTA: SAN V2.2",
-        "version": "2.02.07",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_9521.jpeg",
-        "description": "✅Mod xe dream, khối phân lớn\n✅mod Nhân vật , Còi, Pô, cờ VN, cảnh sát \n",
-        "download_url": "https://drive.google.com/uc?export=download&id=1ffXKKLpTfx9yUWl00FTEr-6DjKRSSd4a",
+        "title": "GTA San N17",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b2.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/N17.jpg",
+        "description": "✅Mod xe Full Dream  \n✅mod Nhân vật Naruto  , Còi, Pô ",
+        "download_url": "https://drive.google.com/uc?export=download&id=1jVw8GTvmfqULAezhNn7Q3KzIpNvkPzPs",
+        "version": "2.02.11",
+        "updated_at": "2025-05-17",
+        "size": "1.68 GB"
+    },
+
+    {
+        "title": "GTA San N16",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b2.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/N16.jpg",
+        "description": "✅Mod xe Full sonic  \n✅mod Nhân vật Naruto  , Còi, Pô ",
+        "download_url": "https://drive.google.com/uc?export=download&id=1j55vQDFFkjSOLR0w_r4j1zstKiNXLUTR",
+        "version": "2.02.11",
+        "updated_at": "2025-05-17",
+        "size": "1.81 GB"
+    },
+
+
+    {
+        "title": "GTA San V17",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V17.jpg",
+        "description": "✅Mod xe Full Dream\n✅mod Nhân vật dân tổ , Còi, Pô ",
+        "download_url": "https://drive.google.com/uc?export=download&id=1JvVcfpQ-kq5QmFE_CxhlHkIQ4dfpbD4r",
+        "version": "2.02.11",
+        "updated_at": "2025-05-17",
+        "size": "1.68 GB"
+    },
+    {
+        "title": "GTA San V16",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/v16.jpg",
+        "description": "✅Mod xe Full sonic  \n✅mod Nhân vật dân tổ , Còi, Pô ",
+        "download_url": "https://drive.google.com/uc?export=download&id=1Hx7O0otojjxAh8AEbhl6SAGoT4revBPv",
+        "version": "2.02.11",
+        "updated_at": "2025-05-17",
+        "size": "1.81 GB"
+    },
+  
+ 
+    {
+        "title": "GTA: SAN V9",
+        "version": "2.02.11",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V9.jpg",
+        "description": "\n✅Mod xe wave \n✅mod Nhân vật dân tổ  , Còi, Pô ",
+        "download_url": "https://www.mediafire.com/file/p3nayagmfn8q560/GTA-_SAN_V9_2.02.11_1690510670.ipa/file",
         "updated_at": "2025-05-15",
-        "size": "1.89 GB"
-    },
-  {
-        "title": "GTA San V12",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-                "image-demo": "https://tiptipios.github.io/chanthuhoi/index/v12.jpg",
-        "description": "✅Mod xe ab ,wave trắng  \n✅mod Nhân vật , Còi, Pô, phóng ",
-        "download_url": "https://drive.google.com/drive/folders/1UYNfrHlxoQ7iFKidbYyvxT8t-ByvluUM?usp=drive_link",
-        "version": "2.02.07",
-        "updated_at": "2024-08-20",
-        "size": "1.96 GB"
+        "size": "1.86 GB"
     },
     {
-        "title": "GTA San V11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-                "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V11.jpg",
-        "description": "✅Mod xe ab ,wave hồng  \n✅mod Nhân vật , Còi, Pô, phóng ",
-        "download_url": "https://drive.google.com/drive/folders/1UYNfrHlxoQ7iFKidbYyvxT8t-ByvluUM?usp=drive_link",
-        "version": "2.02.07",
-        "updated_at": "2024-08-20",
-        "size": "1.96 GB"
-    },
-    {
-        "title": "GTA San V10 ",
-        "version": "2.02.07",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-                "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V10.jpg",
-        "description": "✅Mod xe dream,wave hồng  \n✅mod Nhân vật , Còi, Pô, phóng ",
-        "download_url": "https://drive.google.com/drive/folders/1tZNPm0DcKcP-p8jwOlZUEEo4FggwxyQ2?usp=drive_link",
-        "updated_at": "2024-08-20",
-        "size": "1.96 GB"
-    },
-    {
-        "title": "GTA San V15",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-                "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V15.jpg",
-        "description": "✅Mod xe điện , siêu xe  \n✅mod Nhân vật , Còi, Pô, phóng ",
-        "download_url": "https://drive.google.com/file/d/1v1J_1AZawZbyPQZ_dsF4KwzRPJw286m1/view?usp=drive_link",
-        "version": "2.02.07",
-        "updated_at": "2024-08-20",
-        "size": "1.96 GB"
-    },
-    {
-        "title": "GTA: SAN F6",
+        "title": "GTA: SAN V8",
         "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_0638.jpeg",
-        "description": "✅Mod xe Vario\n✅mod Nhân vật , Còi, Pô",
-        "download_url": "https://drive.google.com/uc?export=download&id=18q3Fs4KUvUH5VBnPbpT0C50zfo8DwvAD",
-        "updated_at": "2024-08-14",
-        "size": "1.79 GB"
-    },
-    {
-        "title": "GTA: SAN F5",
-        "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_0637.jpeg",
-        "description": "✅Mod xe Vario,dream,wave \n✅mod Nhân vật  xăm trổ , Còi, Pô",
-        "download_url": "https://drive.google.com/uc?export=download&id=1tcDLx_UVIEE6ONX6DczR-sHIlvBVzeOV",
-        "updated_at": "2024-08-14",
-        "size": "1.87 GB"
-    },
-    {
-        "title": "GTA: SAN F4",
-        "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
-        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_0636.jpeg",
-        "description": "✅Mod Full Khối phân lớn\n✅mod Nhân vật xăm trổ  , Còi, Pô",
-        "download_url": "https://drive.google.com/uc?export=download&id=1rwJpq94aZklmVNA0Ansagz6K5_3ITS_A",
-        "updated_at": "2024-08-14",
-        "size": "1.85 GB"
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V8.jpg",
+        "description": "\n✅Mod xe wave \n✅mod Nhân vật dân tổ  , Còi, Pô ",
+        "download_url": "https://www.mediafire.com/file/udjoy79o2tq7615/GTA-_SAN_V8_2.02.11_1688735560.ipa/file",
+        "updated_at": "2025-05-15",
+        "size": "1.86 GB"
     },
     {
         "title": "GTA: SAN V7",
         "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
         "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V7.jpg",
         "description": "\n✅Mod xe wave,dream \n✅mod Nhân vật dân tổ  , Còi, Pô ",
         "download_url": "https://drive.google.com/uc?export=download&id=1r5zLzz2lXWks7wpzM8Vjdj5_NBWwIkAz",
@@ -111,28 +136,27 @@ const gtadata = [
     {
         "title": "GTA: SAN V6",
         "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
         "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_9525.jpeg",
         "description": "\n✅Mod xe Vario\n✅mod Nhân vật dân tổ  , Còi, Pô ",
         "download_url": "https://drive.google.com/uc?export=download&id=1lghcNqPxRcrNXF6O_btl8Piy-txniDZ7",
         "updated_at": "2025-05-15",
         "size": "1.62 GB"
     },
-      {
+    {
         "title": "GTA: SAN V5",
         "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
         "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_9527.jpeg",
         "description": "✅Mod xe Vario,dream,wave \n✅mod Nhân vật  dân tổ , Còi, Pô",
         "download_url": "https://drive.google.com/uc?export=download&id=1MwMTxxPnG8ptgXF89XchHhHDoaka4ejf",
         "updated_at": "2025-05-15",
         "size": "1.70 GB"
     },
-    
     {
         "title": "GTA: SAN V4",
         "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
         "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_9528.jpeg",
         "description": "✅Mod Full Khối phân lớn \n✅mod Nhân vật  dân tổ , Còi, Pô",
         "download_url": "https://drive.google.com/uc?export=download&id=1UAefZmgijwfOEu23zTI8ynMPOUQJKeBe",
@@ -142,23 +166,111 @@ const gtadata = [
     {
         "title": "GTA: SAN V3",
         "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
         "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V3.jpg",
         "description": "✅Mod xe dream,wave \n✅mod Nhân vật , Còi, Pô, phóng, cảnh sát ",
         "download_url": "https://drive.google.com/uc?export=download&id=1NxbraoiVtHOYidgV8gy8qYpqgR0xJfha",
         "updated_at": "2025-05-15",
         "size": "2.01 GB"
     },
-      {
+    {
         "title": "GTA: SAN V2",
         "version": "2.02.11",
-        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
         "image-demo": "https://tiptipios.github.io/chanthuhoi/index/v2.jpg",
         "description": "✅Mod xe wave \n✅mod Nhân vật , Còi, Pô, phóng \n",
         "download_url": "https://drive.google.com/uc?export=download&id=1GcTawRPTJmBdIXPzAZ59occkffbJVZe0",
         "updated_at": "2025-05-15",
         "size": "1.95 GB"
+    },
+    {
+        "title": "GTA: SAN V1",
+        "version": "2.02.11",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/v2.jpg",
+        "description": "✅Mod xe wave \n✅mod Nhân vật dân tổ , Còi, Pô \n",
+        "download_url": "https://www.mediafire.com/file/gna8q9ilan5gkf8/GTA-_SA_2.02.11_1690509724.ipa/file",
+        "updated_at": "2025-05-15",
+        "size": "1.95 GB"
+    },
+    {
+        "title": "GTA San V13",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b2.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/v12.jpg",
+        "description": "✅Mod xe Full Wave  \n✅mod Nhân vật naruto , Còi, Pô ",
+        "download_url": "https://drive.google.com/file/d/15DiL3URLqS036E9PMCSrcM88S9uRehTy/view?usp=drive_link",
+        "version": "2.02.07",
+        "updated_at": "2024-08-20",
+        "size": "1.96 GB"
+    },
+    {
+        "title": "GTA San V12",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/v12.jpg",
+        "description": "✅Mod xe ab ,wave trắng  \n✅mod Nhân vật , Còi, Pô, phóng ",
+        "download_url": "https://drive.google.com/drive/folders/1UYNfrHlxoQ7iFKidbYyvxT8t-ByvluUM?usp=drive_link",
+        "version": "2.02.07",
+        "updated_at": "2024-08-20",
+        "size": "1.96 GB"
+    },
+    {
+        "title": "GTA San V11",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V11.jpg",
+        "description": "✅Mod xe ab ,wave hồng  \n✅mod Nhân vật , Còi, Pô, phóng ",
+        "download_url": "https://drive.google.com/drive/folders/1UYNfrHlxoQ7iFKidbYyvxT8t-ByvluUM?usp=drive_link",
+        "version": "2.02.07",
+        "updated_at": "2024-08-20",
+        "size": "1.96 GB"
+    },
+    {
+        "title": "GTA San V10 ",
+        "version": "2.02.07",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V10.jpg",
+        "description": "✅Mod xe dream,wave hồng  \n✅mod Nhân vật , Còi, Pô, phóng ",
+        "download_url": "https://drive.google.com/drive/folders/1tZNPm0DcKcP-p8jwOlZUEEo4FggwxyQ2?usp=drive_link",
+        "updated_at": "2024-08-20",
+        "size": "1.96 GB"
+    },
+    {
+        "title": "GTA San V15",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/V15.jpg",
+        "description": "✅Mod xe điện , siêu xe  \n✅mod Nhân vật , Còi, Pô, phóng ",
+        "download_url": "https://drive.google.com/file/d/1v1J_1AZawZbyPQZ_dsF4KwzRPJw286m1/view?usp=drive_link",
+        "version": "2.02.07",
+        "updated_at": "2024-08-20",
+        "size": "1.96 GB"
+    },
+    {
+        "title": "GTA: SAN F6",
+        "version": "2.02.11",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_0638.jpeg",
+        "description": "✅Mod xe Vario\n✅mod Nhân vật , Còi, Pô",
+        "download_url": "https://drive.google.com/uc?export=download&id=18q3Fs4KUvUH5VBnPbpT0C50zfo8DwvAD",
+        "updated_at": "2024-08-14",
+        "size": "1.79 GB"
+    },
+    {
+        "title": "GTA: SAN F5",
+        "version": "2.02.11",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_0637.jpeg",
+        "description": "✅Mod xe Vario,dream,wave \n✅mod Nhân vật  xăm trổ , Còi, Pô",
+        "download_url": "https://drive.google.com/uc?export=download&id=1tcDLx_UVIEE6ONX6DczR-sHIlvBVzeOV",
+        "updated_at": "2024-08-14",
+        "size": "1.87 GB"
+    },
+    {
+        "title": "GTA: SAN F4",
+        "version": "2.02.11",
+        "image": "https://tiptipios.github.io/chanthuhoi/index/b1.jpg",
+        "image-demo": "https://tiptipios.github.io/chanthuhoi/index/IMG_0636.jpeg",
+        "description": "✅Mod Full Khối phân lớn\n✅mod Nhân vật xăm trổ  , Còi, Pô",
+        "download_url": "https://drive.google.com/uc?export=download&id=1rwJpq94aZklmVNA0Ansagz6K5_3ITS_A",
+        "updated_at": "2024-08-14",
+        "size": "1.85 GB"
     }
-    
-  
 ];
