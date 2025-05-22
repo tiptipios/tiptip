c3b4cceed265e525e7b7be632d5972ddec85976e
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-05-22 23:55:30 +0700

    Add files via upload

--- js/tiptip.js
+++ js/tiptip.js
@@ -1,4 +1,5 @@
 const gamedata = [
+
      {
         "title": "The Spike",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
@@ -8,6 +9,17 @@ const gamedata = [
         "version": "5.9.405",
         "updated_at": "2025-05-22",
         "size": "238.87 MB"
+    },
+       {
+        "title": "Liên Quân Mobile",
+        "version": "1.58.11379483",
+        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/f399da84-52b5-4dfc-82bb-b3a99ce8ce72.image.png?v=1681997379317%27",
+        "image-demo": "https://tiptipios.github.io/ios/IMG_1270.jpeg",
+        "description": "✅Map Full HD Ko che \n ✅Cam xa ( 3 chế độ) \n ✅Hiện until  \n ✅Hiện bổ trợ \n ✅Hiện Avatar \n ✅Hiện rank  \n ✅Hiện lịch sử đấu  \n ✅ Menu Mod Skin,Sảnh (tap 3 ngón tay 2 lần )\n ✅ Kín = Bất tử ",
+        "download_url": "https://drive.usercontent.google.com/download?id=1Cgh8rsHE0glxJU83e0ilD7WvjThs4Eqj",
+        "updated_at": "2025-05-22",
+        "size": "170 MB",
+        "fake_downloads": 9999999
     },
     {
         "title": "Jurassic World",
