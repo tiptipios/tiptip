03dc3f062fbe27f8baab753d3f73b9bfcec87e65
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-06-06 08:29:53 +0700

    Add files via upload

--- js/spike.js
+++ js/spike.js
@@ -1,9 +1,19 @@
 const spikedata = [
     {
+        "title": "The Spike V1",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
+        "description": "✅ Siwoo S  ( Siwoo B )\n✅ Raul  ( Youngsub )\n✅ Nishikawa Highschool  ( Hongsi )\n✅ Skill Buff chỉ số Team\n✅ Buff All Nhân vật ( 50 đ nhảy,nhanh nhẹn và 100 đ thủ  )\n✅ Skips Ads\n✅ Điều kiển bóng trong trận ( chữ TipTip góc trái )",
+        "download_url": "https://drive.google.com/file/d/1EVB9C_tWLlTyrzTtoArh0AU3sXtSbVuN/view?usp=drive_link",
+        "version": "5.9.405",
+                "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
+        "updated_at": "2025-06-06",
+        "size": "238.81 MB"
+    },
+    {
         "title": "The Spike Mod Siwoo",
         "image": "https://tiptipios.github.io/ios/sp_illustFaceIcon/141.PNG",
         "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/14.jpg",
-        "description": "✅ Mod nhân Vật Siwoo ( Siwoo B )\n✅ Buff Nhân vật chính (đột phá sẽ tăng thêm chỉ số tấn công)  \n✅ Buff All Nhân vật khác (50đ nhảy,nhanh nhẹn và 100đ Thủ)\n✅ Skips Ads\n✅ Điều kiển bóng trong trận ( chữ TipTip góc trái )\n✅ Cam cao",
+        "description": "✅ Mod nhân Vật Siwoo  ( Siwoo B )\n✅ Buff Nhân vật chính (đột phá sẽ tăng thêm chỉ số tấn công)  \n✅ Buff All Nhân vật khác (50đ nhảy,nhanh nhẹn và 100đ Thủ)\n✅ Skips Ads\n✅ Điều kiển bóng trong trận ( chữ TipTip góc trái )\n✅ Cam cao",
         "download_url": "https://drive.google.com/file/d/1y74L5IlaX7nAqCbToktfIvNkVA4cWxwd/view",
         "version": "5.9.405",
         "updated_at": "2025-05-31",
