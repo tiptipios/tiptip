171500ef31e1ceba74fd1e6fcfb81a0cc1786fed
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-06-24 09:05:14 +0700

    Update spike.js

--- js/spike.js
+++ js/spike.js
@@ -8,6 +8,16 @@ const spikedata = [
         "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
         "updated_at": "2025-06-20",
         "size": "238.81 MB"
+    },
+    {
+        "title": "The Spike Mod Android",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
+        "description": "✅ Siwoo -> Raul\n✅ Buff Nhân vật Siwoo ( 120 đ công, 50 đ nhảy,nhanh nhẹn và 100 đ thủ  )\n✅ No Ads",
+        "download_url": "https://oklink.cfd/ppAW",
+        "version": "6.0.3",
+        "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
+        "updated_at": "2025-06-20",
+        "size": "238.81 MB"
     }
     
 ];
