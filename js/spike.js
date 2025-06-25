2afe0cd095129f4f00b0c7016c731de21f8311ea
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-06-25 09:47:25 +0700

    Update spike.js

--- js/spike.js
+++ js/spike.js
@@ -1,4 +1,14 @@
 const spikedata = [
+     {
+        "title": "The Spike V2",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
+        "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅ Skill Buff chỉ số Team ( Có trên các nhân vật $ Ws )\n✅ Buff Nhân vật Siwoo ( 120 đ thủ, công. 40đ nhảy,nhanh nhẹn)\n✅ No Ads\n✅ Mod bóng gốc.\n✅ Điều kiển bóng trong trận ( chữ TipTip góc trái ) \n✅ Đã fix Raul bị văng khi ddapj vào chắn",
+        "download_url": "https://www.mediafire.com/file/yenb0wrs4hfamxj/The_Spike_TipTip_6.0.3.ipa/file",
+        "version": "6.0.3",
+        "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
+        "updated_at": "2025-06-25",
+        "size": "238.81 MB"
+    },
     {
         "title": "The Spike V1",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
