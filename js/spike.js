369748da24913d2dd9f855e6010f255d48e9b18e
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-08-15 12:39:34 +0700

    Add files via upload

--- js/spike.js
+++ js/spike.js
@@ -1,12 +1,12 @@
 const spikedata = [
     {
-        "title": "The Spike V1",
+        "title": "The Spike",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ Menu Mod Nhân Vật Siwoo \n✅ Buff Nhân vật Siwoo ( 120 đ công, 50 đ nhảy,nhanh nhẹn và 100 đ thủ  )\n✅ No Ads\n✅ Mod bóng gốc.\n✅ Điều kiển bóng trong trận ( chữ TipTip góc trái ) \n✅ Đã fix Raul bị văng khi ddapj vào chắn",
-        "download_url": "https://oklink.cfd/jJIo",
-        "version": "6.0.3",
+        "description": "✅  Mod Nhân Vật\n✅Siwoo, Oasis -> Lucas\n✅Ryuhyun -> Nishikawa\n✅ Clyde-> Crow\n✅Atis -> Hari\n✅Sohee -> Zero\n✅Lisia -> Zero\n✅ No Ads",
+        "download_url": "https://oklink.cfd/sgt",
+        "version": "6.2.105",
         "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
-        "updated_at": "2025-06-23",
+        "updated_at": "2025-08-15",
         "size": "238.81 MB"
     }
 ];
