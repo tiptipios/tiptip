bbc5040752a74e634763a2065e5c90501a2b7f3f
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-08-15 12:31:06 +0700

    Add files via upload

--- js/spike.js
+++ js/spike.js
@@ -1,14 +1,4 @@
 const spikedata = [
-     {
-        "title": "The Spike Ver 2.2",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ $ 50k/tháng\n✅  Menu Mod Nhân Vật tuỳ chọn\n✅ Skill Buff chỉ số Team ( Có trên các nhân vật $ Ws )\n✅ Buff Nhân vật Siwoo ( 120 đ thủ, công. 40đ nhảy,nhanh nhẹn)\n✅ No Ads\n✅ Cam cao ( cài đặt 50% = Cam gốc)\n✅ Mod bóng gốc ( hiệu ứng bung tiền, Nuôi pet )\n✅  Buff Chỉ số Nhân vật dưới S\n✅  Aura khi bật Skill TipTip\n✅ Tuỳ chỉnh màu Aura \n✅ Thêm nhân vật Goku super saiyan blue kaioken\n✅  cài song song bản gốc\n✅😈 Ai đã và đang sử dụng V2 hoặc v2.1 thì cứ cài bản v2.2 này. Cài xong thì xoá v2,2.1 thì sẽ ko bị reset  udid  \n✅💀 không nên sử dụng acc chính ",
-        "download_url": "https://oklink.cfd/z8ly",
-        "version": "6.0.3",
-        "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
-        "updated_at": "2025-06-28",
-        "size": "238.81 MB"
-    },
     {
         "title": "The Spike V1",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
@@ -18,16 +8,5 @@ const spikedata = [
         "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
         "updated_at": "2025-06-23",
         "size": "238.81 MB"
-    },
-    {
-        "title": "The Spike Mod Android",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ Siwoo -> Raul\n✅ Buff Nhân vật Siwoo ( 120 đ công, 50 đ nhảy,nhanh nhẹn và 100 đ thủ  )\n✅ No Ads",
-        "download_url": "https://oklink.cfd/ppAW",
-        "version": "6.0.3",
-        "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
-        "updated_at": "2025-06-24",
-        "size": "238.81 MB"
     }
-    
 ];
