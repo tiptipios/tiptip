9ebc02b8162c3eabe2830680881df3be89fefd98
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-09-06 16:08:13 +0700

    Update spike.js

--- js/spike.js
+++ js/spike.js
@@ -1,12 +1,13 @@
 const spikedata = [
     {
-        "title": "The Spike",
+        "title": "The Spike New Update",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅  Mod Nhân Vật\n✅Siwoo, Oasis -> Lucas\n✅Ryuhyun -> Nishikawa\n✅ Clyde-> Crow\n✅Atis -> Hari\n✅Sohee -> Zero\n✅Lisia -> Zero\n✅ No Ads",
-        "download_url": "https://oklink.cfd/sgt",
-        "version": "6.2.105",
+        "description": "✅ Mod Nhân Vật ✅ Cam cao✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )✅ AutoWin( My+,En+,Ball ) - không Lạm dụng✅ No Ads",
+        "download_url": "https://oklink.cfd/d49J",
+        "version": "6.2.121",
         "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
-        "updated_at": "2025-08-15",
+        "updated_at": "2025-09-06",
         "size": "238.81 MB"
     }
+
 ];
