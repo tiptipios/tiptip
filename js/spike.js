47c535e17459f7ecd3edd0ca9f1dc16287236996
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-09-14 11:31:11 +0700

    Update spike.js

--- js/spike.js
+++ js/spike.js
@@ -8,6 +8,17 @@ const spikedata = [
         "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
         "updated_at": "2025-09-06",
         "size": "238.81 MB"
+    },
+     {
+        "title": "The Spike New V1",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
+        "description": "✅ Menu Mod Nhân Vật tuỳ chọn ✅ Cam cao  ✅  Buff Chỉ số Nhân vật tuỳ chỉnh ✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) ✅ AutoWin( My+,En+,Ball ) - không Lạm dụng ✅ No Ads ✅  cài song song bản gốc ✅ Key test: TipTip-T.me/gamemodios ( đến 16/9) ✅🔓 phí 50k/tháng 💵",
+        "download_url": "https://drive.google.com/file/d/14wf3HNrTx-Uk9zgRfneH-_2ISU5TTdA1/view?usp=drivesdk",
+        "version": "6.2.121",
+        "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
+        "updated_at": "2025-09-06",
+        "size": "238.81 MB"
     }
 
 ];
+
