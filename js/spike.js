65ad5ca6f18293187bc3654214da30570c635a93
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-09-30 11:32:17 +0700

    Add files via upload

--- js/spike.js
+++ js/spike.js
@@ -2,30 +2,10 @@ const spikedata = [
     {
         "title": "The Spike New V1.0",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )\n✅ AutoWin( My+,En+,Ball ) - không Lạm dụng\n✅ No Ads\n✅ Cài song song bản gốc\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵",
-        "download_url": "https://drive.google.com/file/d/1G6QMs-p-Q7fAz9MGnmWIu2aOMw8lwtKt/view?usp=drivesdk",
-        "version": "6.3.202",
-        "updated_at": "2025-09-23",
-        "size": "238.81 MB"
-    },
-    {
-        "title": "The Spike  V2",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )\n✅ AutoWin( My+,En+,Ball ) - không Lạm dụng\n✅ No Ads\n✅ Cài song song bản gốc\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n✅🔓 phí 50k/tháng 💵",
-        "download_url": "https://drive.google.com/file/d/1PTMW6UgeWkf2hPY5OMKRGZl6L3so3aY-/view?usp=drivesdk",
-        "version": "6.2.121",
-        "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
-        "updated_at": "2025-09-23",
-        "size": "238.81 MB"
-    },
-    {
-        "title": "The Spike  ",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ Mod Nhân Vật \n ✅ Cam cao \n ✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n ✅ AutoWin( My+,En+,Ball ) - không Lạm dụng \n ✅ No Ads",
-        "download_url": "https://oklink.cfd/d49J",
-        "version": "6.2.121",
-        "image-demo": "https://tiptipios.github.io/ios/sp_illustFaceIcon/Ver1.jpg",
-        "updated_at": "2025-09-23",
+        "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )\n✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n✅ Unlock Style Ball\n✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵",
+        "download_url": "https://drive.google.com/file/d/1pRcRnGFQHWYTBIaX6xtOHMHUaAfH8Lem/view?usp=drivesdk",
+        "version": "6.3.307",
+        "updated_at": "2025-09-30",
         "size": "238.81 MB"
     }
 ];
