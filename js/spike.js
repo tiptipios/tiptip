b4c207dfd3915909ed7d5ac866a85a2eee4171a0
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-10-17 12:59:09 +0700

    Add files via upload

--- js/spike.js
+++ js/spike.js
@@ -1,5 +1,14 @@
 const spikedata = [
     {
+        "title": "The Spike New V3.0",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
+        "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕 ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )\n✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) để đọc hướng dẫn cách dùng",
+        "download_url": "https://www.mediafire.com/file/dsroyrjby1o6ide/The_Spike_V3_6.3.310.ipa/file",
+        "version": "6.3.310",
+        "updated_at": "2025-10-17",
+        "size": "238.81 MB"
+    },
+    {
         "title": "The Spike New V2.0",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
         "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )\n✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n✅ Unlock Style Ball\n✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) để đọc hướng dẫn cách dùng",
@@ -17,5 +26,4 @@ const spikedata = [
         "updated_at": "2025-09-30",
         "size": "238.81 MB"
     }
-
 ];
