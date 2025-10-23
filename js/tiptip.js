6ecd3305ccad4b052f7f69a63bfc9b5a23ca3b86
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-10-23 21:55:45 +0700

    Update tiptip.js

--- js/tiptip.js
+++ js/tiptip.js
@@ -1,12 +1,12 @@
 const gamedata = [
     {
         "title": "Liên Quân Mobile",
-        "version": "1.59.1.4",
+        "version": "1.69.115",
         "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/f399da84-52b5-4dfc-82bb-b3a99ce8ce72.image.png?v=1681997379317%27",
         "image-demo": "https://tiptipios.github.io/ios/IMG_1270.jpeg",
         "description": "✅Map Full HD Ko che \n ✅Cam xa ( 3 chế độ) \n ✅Hiện until  \n ✅Hiện bổ trợ \n ✅Hiện Avatar \n ✅Hiện rank  \n ✅Hiện lịch sử đấu  \n ✅ Menu Mod Skin,Sảnh (tap 3 ngón tay 2 lần )\n ✅ Kín = Bất tử ",
-        "download_url": "https://oklink.cfd/3vix",
-        "updated_at": "2025-07-09",
+        "download_url": "https://oklink.cfd/BCBS",
+        "updated_at": "2025-10-23",
         "size": "170 MB"
     },
     {
@@ -2342,4 +2342,5 @@ const gamedata = [
         "updated_at": "2025-02-20",
         "size": "192.15 MB"
     }
+
 ];
