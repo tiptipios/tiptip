bf232508ee46853ce52cab7de4e7d42f63f5278a
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-11-02 14:22:49 +0700

    Update spike.js

--- js/spike.js
+++ js/spike.js
@@ -2,7 +2,7 @@ const spikedata = [
     {
         "title": "The Spike New V4.0",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅🤤Free \n✅ Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí  Menu Mod Nhân Vật tuỳ chọn \n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕 ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n \n✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) để đọc hướng dẫn cách dùng",
+        "description": "✅🤤Free \n✅ Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n Menu Mod Nhân Vật tuỳ chọn \n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n ✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
         "download_url": "https://www.mediafire.com/file/b6ldw9opaw6q5em/The_Spike_V4_6.3.330.ipa/file",
         "version": "6.3.330",
         "updated_at": "2025-11-02",
@@ -37,3 +37,4 @@ const spikedata = [
     }
 
 ];
+
