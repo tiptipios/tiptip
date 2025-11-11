fd489d7c881136a9aa846b6006674be55fee31d7
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-11-11 15:12:25 +0700

    Add files via upload

--- js/spike.js
+++ js/spike.js
@@ -1,47 +1,11 @@
 const spikedata = [
     {
-        "title": "The Spike New V5.0",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅🤤Free \n✅ Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n Menu Mod Nhân Vật tuỳ chọn \n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
-        "download_url": "https://www.mediafire.com/file/w8jfbqq19z9host/The_Spike_V5_6.3.338.ipa/file",
-        "version": "6.3.338",
-        "updated_at": "2025-11-06",
-        "size": "238.81 MB"
-    },
-    {
-        "title": "The Spike New V4.0",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅🤤Free \n✅ Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n Menu Mod Nhân Vật tuỳ chọn \n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n ✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
-        "download_url": "https://www.mediafire.com/file/b6ldw9opaw6q5em/The_Spike_V4_6.3.330.ipa/file",
-        "version": "6.3.330",
-        "updated_at": "2025-11-02",
-        "size": "238.81 MB"
-    },
-    {
-        "title": "The Spike New V3.0",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕 ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )\n✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) để đọc hướng dẫn cách dùng",
-        "download_url": "https://www.mediafire.com/file/dsroyrjby1o6ide/The_Spike_V3_6.3.310.ipa/file",
-        "version": "6.3.310",
-        "updated_at": "2025-10-17",
-        "size": "238.81 MB"
-    },
-    {
-        "title": "The Spike New V2.0",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )\n✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n✅ Unlock Style Ball\n✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) để đọc hướng dẫn cách dùng",
-        "download_url": "https://drive.usercontent.google.com/download?id=1uxx9-I0ozo0PQREAAXr95cWeBWiiCyX0&export=download&authuser=1",
-        "version": "6.3.310",
-        "updated_at": "2025-10-12",
-        "size": "238.81 MB"
-    },
-    {
         "title": "The Spike New V1.0",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅ Menu Mod Nhân Vật tuỳ chọn \n✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅ Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung )\n✅ AutoWin( My+,En+,Ball ) -Lạm dụng nhiều sẽ bị ban tk  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n✅ Unlock Style Ball\n✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ Sẽ tự động buff chỉ số cho nhân vật được mod ( Mod Nv2). Nên dùng cái này vì nó sẽ load cùng Mod NV2 ( Nhanh hơn )\n\n✅ Mod NV2 \n✅ Tự động hiển thị những nhân vật hiện có thay vì hiện tất cả như trước.  \n\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵",
-        "download_url": "https://drive.google.com/file/d/1pRcRnGFQHWYTBIaX6xtOHMHUaAfH8Lem/view?usp=drivesdk",
-        "version": "6.3.307",
-        "updated_at": "2025-09-30",
+        "description": "✅🤤Free \n✅ Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n Menu Mod Nhân Vật tuỳ chọn \n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
+        "download_url": "https://www.mediafire.com/file/8tconetnpb3049a/The_Spike_V1_6.3.350.ipa/file",
+        "version": "6.3.350",
+        "updated_at": "2025-11-11",
         "size": "238.81 MB"
     }
 ];
