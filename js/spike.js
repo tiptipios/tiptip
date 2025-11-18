f25d14b39c76505284d080bf1b0423f7cd3bf97c
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-11-18 13:51:25 +0700

    Add files via upload

--- js/spike.js
+++ js/spike.js
@@ -1,5 +1,14 @@
 const spikedata = [
     {
+        "title": "The Spike New V2.0",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
+        "description": "✅🤤Free \n✅   Mod Nhân Vật WS ( Thêm Hanra, Dahee )\n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n ✅  Mod Nhân Vật tuỳ chọn 🔄\n+ Thêm 4 nhân Vật trong giải đấu ( Seohlwa MB, WildLeon, WildViole, EnemySanghyeon )✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
+        "download_url": "https://www.mediafire.com/file/4s11b5lol0sr4rj/The_Spike_V2_6.3.350_1763443602.ipa/file",
+        "version": "6.3.350",
+        "updated_at": "2025-11-18",
+        "size": "238.81 MB"
+    },
+    {
         "title": "The Spike New V1.0",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
         "description": "✅🤤Free \n✅ Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n Menu Mod Nhân Vật tuỳ chọn \n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
