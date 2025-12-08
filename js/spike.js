e0790b0741e30ddf31c4a6fad10c58dee2f05228
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-12-08 11:29:39 +0700

    Update spike.js

--- js/spike.js
+++ js/spike.js
@@ -1,4 +1,13 @@
 const spikedata = [
+          {
+        "title": "The Spike New V2.1",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
+        "description": "✅🤤Free \n✅   Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n ➕ Hiệu ứng Raul không giới hạn ( Raul , Raul_Enemy_Invincible ) \n ✅  Mod All Nhân Vật tuỳ chọn 🔄\n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( thêm claire,sif. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
+        "download_url": "https://www.mediafire.com/file/gm2s8kd42z9ng1m/The_Spike_V2.1_6.4.407.ipa/file",
+        "version": "6.4.407",
+        "updated_at": "2025-12-08",
+        "size": "238.81 MB"
+    },
       {
         "title": "The Spike New V2.0",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
@@ -19,4 +28,5 @@ const spikedata = [
     }
 
 ];
+
 
