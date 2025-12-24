2597e0f4c0b26cf690f8b741155a53720765b001
tiptipios <131225223+tiptipios@users.noreply.github.com>
2025-12-24 16:05:17 +0700

    Add files via upload

--- js/spike.js
+++ js/spike.js
@@ -1,33 +1,11 @@
 const spikedata = [
-          {
-        "title": "The Spike New V2.1",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅🤤Free \n✅   Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n ✅➕ Hiệu ứng Raul không giới hạn ( Raul , Raul_Enemy_Invincible ) \n ✅  Mod All Nhân Vật tuỳ chọn 🔄\n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( thêm claire,sif. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
-        "download_url": "https://www.mediafire.com/file/gm2s8kd42z9ng1m/The_Spike_V2.1_6.4.407.ipa/file",
-        "version": "6.4.407",
-        "updated_at": "2025-12-08",
-        "size": "238.81 MB"
-    },
-      {
-        "title": "The Spike New V2.0",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅🤤Free \n✅   Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n ✅  Mod All Nhân Vật tuỳ chọn 🔄\n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( thêm claire,sif. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
-        "download_url": "https://www.mediafire.com/file/mfeh86q9a7hjfvn/The_Spike_V2_6.4.407.ipa/file",
-        "version": "6.4.407",
-        "updated_at": "2025-12-05",
-        "size": "238.81 MB"
-    },
     {
-        "title": "The Spike New V1.0",
+        "title": "The Spike New V3",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
-        "description": "✅🤤Free \n✅   Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n ✅  Mod All Nhân Vật tuỳ chọn 🔄\n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( ngon hơn. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
-        "download_url": "https://www.mediafire.com/file/tp4sayipwcazd37/The_Spike_V1_6.4.406.ipa/file",
-        "version": "6.4.406",
-        "updated_at": "2025-12-04",
+        "description": "✅🤤Free \n✅   Mod Nhân Vật WS \n✅  Không hồi chiêu ( Lucas, Sanghyeon, Zero, Heesung ) \n✅👑  Trả Phí \n ✅➕Unlimited Effect ( Raul, Hongsi, Nishi_Black\n ) \n ✅  Mod All Nhân Vật tuỳ chọn 🔄\n✅  Mod Skin ball 🆕\n✅ Mod Map🆕\n✅  Mod Siwoo🆕\n ✅ Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n  \n✅ No Ads\n✅ Cài song song bản gốc  No Win\n ✅ No Cd 2 ( thêm claire,sif. )\n✅ Unlock Skill AI ( Raul,Nishi,oasis,yongsub,...)\n\n✅ Buff chỉ số cho nhân vật mod\n✅ không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường nó sẽ đè lên bản cũ\n\n ✅🔓 phí 50k/tháng 💵n\n ✅Nhấn (i) trogn menu để đọc hướng dẫn cách dùng",
+        "download_url": "https://www.mediafire.com/file/9a46hw2lujb76s5/The_Spike_V3_6.5.101_1766566155.ipa/file",
+        "version": "6.5.101",
+        "updated_at": "2025-12-24",
         "size": "238.81 MB"
     }
-
 ];
-
-
-
