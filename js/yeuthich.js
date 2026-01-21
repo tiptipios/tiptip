02b5a056dbaed9c65b15d608856ff98aa80627a2
tiptipios <131225223+tiptipios@users.noreply.github.com>
2026-01-22 00:17:29 +0700

    Update yeuthich.js

--- js/yeuthich.js
+++ js/yeuthich.js
@@ -2,12 +2,12 @@ const yeuthichdata = [
      
       {
         "title": "Liên Quân Mobile",
-        "version": "1.60.115",
+        "version": "1.61.11572693",
         "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/f399da84-52b5-4dfc-82bb-b3a99ce8ce72.image.png?v=1681997379317%27",
         "image-demo": "https://tiptipios.github.io/ios/IMG_1270.jpeg",
         "description": "✅Map Full HD Ko che \n ✅Cam xa ( 3 chế độ) \n ✅Hiện until  \n ✅Hiện bổ trợ \n ✅Hiện Avatar \n ✅Hiện rank  \n ✅Hiện lịch sử đấu  \n ✅ Menu Mod Skin,Sảnh (tap 3 ngón tay 2 lần )\n ✅ Kín = Bất tử ",
-        "download_url": "https://oklink.cfd/BCBS",
-        "updated_at": "2025-10-22",
+        "download_url": "https://oklink.cfd/r79Pm",
+        "updated_at": "2026-01-22",
         "size": "170 MB",
         "fake_downloads": 9999999
     },
@@ -72,5 +72,6 @@ const yeuthichdata = [
         "size": "1104.66 MB",
     }
 ];
+
 
 
