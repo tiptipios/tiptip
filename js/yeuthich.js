60bf0b5226f709e8c9c2f7f77a83d5e516e24f50
tiptipios <131225223+tiptipios@users.noreply.github.com>
2026-02-07 04:39:19 +0700

    Update game data and timestamps in yeuthich.js
    Updated the 'updated_at' field for 'Liên Quân Mobile' and added new entries for 'CarX Street'.

--- js/yeuthich.js
+++ js/yeuthich.js
@@ -7,7 +7,7 @@ const yeuthichdata = [
         "image-demo": "https://tiptipios.github.io/ios/IMG_1270.jpeg",
         "description": "✅Map Full HD Ko che \n ✅Cam xa ( 3 chế độ) \n ✅Hiện until  \n ✅Hiện bổ trợ \n ✅Hiện Avatar \n ✅Hiện rank  \n ✅Hiện lịch sử đấu  \n ✅ Menu Mod Skin,Sảnh (tap 3 ngón tay 2 lần )\n ✅ Kín = Bất tử ",
         "download_url": "https://oklink.cfd/r79Pm",
-        "updated_at": "2026-01-22",
+        "updated_at": "2026-02-07",
         "size": "170 MB",
         "fake_downloads": 9999999
     },
@@ -72,6 +72,7 @@ const yeuthichdata = [
         "size": "1104.66 MB",
     }
 ];
+
 
 
 
