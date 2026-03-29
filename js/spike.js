1a27a6266e2cf0a2ab4381f1a608c8b4f7a13db2
tiptipios <131225223+tiptipios@users.noreply.github.com>
2026-03-29 12:18:33 +0700

    Modify spikedata with new download URL and version
    Updated download URL and version for spikedata.

--- js/spike.js
+++ js/spike.js
@@ -3,9 +3,9 @@ const spikedata = [
         "title": "The Spike New ( TSC•Tip Tip)",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
         "description": "🤤Free \n✅  Mod Nhân Vật WS \n✅  Không hồi chiêu V1\n✅  No Ads\n✅  Cài song song bản gốc\n👑  Vip 1 👑\n✅  Mod All Nhân Vật/Skin tuỳ chọn \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅  Mod Skin ball\n✅  Mod Map\n✅  Unlimited Effect \n✅  Unlock Skill AI \n✅  Cam cao \n✅  Buff Chỉ số Nhân vật tuỳ chỉnh\n✅  không thời gian hồi chiêu V2\n👑  Vip 2 👑\n✅  My+,En+,Ball ⚠️\n✅  No Win ⚠️\n😈 không xoá bản cũ nếu đã được duyệt udid. Cứ cài bình thường bản mới nó sẽ đè lên bản cũ Rồi chơi tiếp nhé.\n• 🔓 Free : không cần duyệt 🆓\n• 🔓 Vip1 : phí 60k/tháng 💵\n• 🔓 Vip2 : phí 100k/tháng 💵",
-        "download_url": "https://oklink.cfd/xfwjkB",
-        "version": "7.0.102",
-        "updated_at": "2026-03-27",
+        "download_url": "https://oklink.cfd/jhtQDy",
+        "version": "7.0.105",
+        "updated_at": "2026-03-29",
         "size": "438.81 MB"
     }
 
